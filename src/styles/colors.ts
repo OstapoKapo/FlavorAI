39b5c294c096e47c9460c99f5b0c7c20da1e75b3
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-10-06 13:35:40 +0300

    start: create structure

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  backgroundGradient: ['#ffe5d9', '#ffd6a5', '#fdffb6', '#caffbf'],
+  textPrimary: '#3a3a3a',
+  textHeading: '#2c1a0b',
+  btnPrimary: '#ff7f50',
+  btnPrimaryHover: '#ff6b3d',
+};
+
+export default colors;
