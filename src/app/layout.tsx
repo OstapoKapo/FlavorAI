383411c94959dbcd392a4ec543ff37021beaee12
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-10-06 13:49:04 +0300

    start: end preparation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const FontMontserrat = Montserrat({
-  variable: "--font-montserrat",
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "FlavorAI",
-  description: "AI-powered recipe generator",
+  description: "create or find recipes based on ingredients you have",
+  keywords: ["AI", "recipes", "ingredients"],
   icons: {
-    icon: "/icons/favicon.ico",
+    icon: "/icons/favicon.png",
   },
-  authors: [{ name: "OstapoKapo" }],
 };
 
 export default function RootLayout({
@@ -23,7 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${FontMontserrat.variable} `}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
